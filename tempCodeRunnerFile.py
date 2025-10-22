c4082657769366bf8fadc235aff7b176e69d3df3
Breno Cardoso <dev.brenosspc@gmail.com>
2025-10-22 17:00:42 -0300

    Update tempCodeRunnerFile.py
    finish

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -9,7 +9,30 @@ def ver_tarefas(tarefas):
     for indice, tarefa in enumerate(tarefas, start=1):
         status = "✓" if tarefa["completada"] else " "
         nome_tarefa = tarefa ["tarefa"]
-        print(f"{indice}. [{status}] {tarefa}")
+        print(f"{indice}. [{status}] {nome_tarefa}")
+    return
+
+def atualizar_none_tarefa(tarefas, indice_tarefa, novo_nome_tarefa):
+    indice_tarefa_ajustado = int(indice_tarefa) - 1
+    if indice_tarefa_ajustado >= 0 and indice_tarefa_ajustado < len(tarefas):
+        tarefas[indice_tarefa_ajustado] ["tarefa"] = novo_nome_tarefa
+        print(f"Tarefa {indice_tarefa} atualizada para {novo_nome_tarefa}")
+    else:
+        print("Indice de tarefa invalido")
+    return
+
+def completar_tarefa(tarefas, indice_tarefa):
+    indice_tarefa_ajustado = int(indice_tarefa) - 1
+    tarefas[indice_tarefa_ajustado]["completada"] = True
+    print(f"Tarefa {indice_tarefa} marcada como completada")
+    return
+
+def deletar_tarefa_completada(tarefas):
+    for tarefa in tarefas:
+        if tarefa["completada"]:
+            tarefas.remove(tarefa)
+    print(f"Tarefas completadas foram deletadas")
+    return
 
 tarefas = []
 while True:
@@ -26,8 +49,26 @@ while True:
     if escolha == "1":
         nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
         adicionar_tarefa(tarefas, nome_tarefa)
+
     elif escolha == "2":
-        ver_tarefas(tarefas)    
+        ver_tarefas(tarefas)
+
+    elif escolha == "3":
+        ver_tarefas(tarefas)
+        indice_tarefa = input ("Digite o numero da tarefa que deseja atualizar: ")
+        novo_nome = input ("Digite o novo nome da tarefa: ")
+        atualizar_none_tarefa(tarefas, indice_tarefa, novo_nome)
+
+    elif escolha == "4":
+        ver_tarefas(tarefas)
+        indice_tarefa = input ("Digite o numero da tarefa que deseja completar")
+        completar_tarefa(tarefas, indice_tarefa)
+
+    elif escolha == "5":
+        deletar_tarefa_completada(tarefas)
+        ver_tarefas(tarefas)
+
+
     elif escolha == "6":
         break
 
